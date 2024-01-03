d9af22aacc2dd0ff0a088a06c93a758f053ed4cf
GoncharenkoVitaliy <Vitalesss.87@gmail.com>
2024-01-03 21:08:05 +0200

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import SearchPages from "./pages/SearchPages";
 import FavouritesPage from "./pages/FavouritesPage";
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="flex flex-col mx-0 px-0 w-full  bg-gray-300">
+    <div className="flex flex-col min-h-full mx-0 px-0 w-full  bg-gray-300">
       <Navigation />
-      <Routes>
-        <Route path="/" element={<SearchPages />} />
-        <Route path="/favourites" element={<FavouritesPage />} />
-      </Routes>
+      <div  className="flex-[1_1_0%]">
+        <Routes>
+          <Route path="/" element={<SearchPages />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+        </Routes>
+      </div>
+      <div className="flex-[0_0_0%]"><Footer /></div>
     </div>
   );
 }
