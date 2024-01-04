446cc403ade2c34a29a562db6545c75097bf5149
GoncharenkoVitaliy <Vitalesss.87@gmail.com>
2024-01-04 21:38:18 +0200

    footerPaddings

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="flex items-center justify-center mx-0 px-0 w-full h-[60px] shadow-md bg-gray-400 text-white">
+    <div className="flex items-center justify-center mx-0 px-5 py-10 w-full h-[60px] shadow-md bg-gray-400 text-white">
       Telegram: "https://t.me/VitaliyGoncharenkoPMR"
     </div>
   );
